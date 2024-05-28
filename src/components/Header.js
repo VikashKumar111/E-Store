2f94a0baa212a6ba42f8da3f049e20eee1e78a1d
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-28 16:03:25 +0530

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Logo from "./Logo";
 
 const Header = () => {
-    return <div>Header</div>;
+    return <header>
+        <div className="container mx-auto">
+            <div className="">
+                <Logo w={90} h={60} />
+            </div>
+        </div>
+    </header>;
 };
 
 export default Header;
